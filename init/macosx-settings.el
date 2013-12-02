d0559b3aa0094e4a069a2e96361b7e2c7890ea42
Jun Yabe <yabe@auckland.local>
2013-12-02 16:05:44 +0900

    update macosx-settings.el for fonts

--- init/macosx-settings.el
+++ init/macosx-settings.el
@@ -2,27 +2,19 @@
 ;; Switch meta and command      
 (setq ns-command-modifier (quote meta))
 (setq ns-alternate-modifier (quote super))
-(when (>= emacs-major-version 23)
+
+;; English
  (set-face-attribute 'default nil
-                     :family "monaco"
-                     :height 140)
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0208
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0212
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'mule-unicode-0100-24ff
-  '("monaco" . "iso10646-1"))
- (setq face-font-rescale-alist
-      '(("^-apple-hiragino.*" . 1.2)
-        (".*osaka-bold.*" . 1.2)
-        (".*osaka-medium.*" . 1.2)
-        (".*courier-bold-.*-mac-roman" . 1.0)
-        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-        (".*monaco-bold-.*-mac-roman" . 0.9)
-        ("-cdac$" . 1.3))))
+             :family "Menlo" ;; font
+             :height 120)    ;; font size
+
+;; Japanese
+(set-fontset-font
+ nil 'japanese-jisx0208
+;; (font-spec :family "Hiragino Mincho Pro")) ;; font
+  (font-spec :family "Hiragino Kaku Gothic ProN")) ;; font
+
+;; 
+(setq face-font-rescale-alist
+      '((".*Hiragino_Mincho_pro.*" . 1.2)))
+

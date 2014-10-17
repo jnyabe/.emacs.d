1a90b2683b7e8d60be0e93e15f4a17cedb353943
Jun Yabe <yabe@auckland.local>
2014-10-17 12:40:33 +0900

    Fix a problem with yasnippet/initialize

--- init/08-yasnippet.el
+++ init/08-yasnippet.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path
 	     "~/.emacs.d/plugins/yasnippet-0.6.1c")
 (require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+;;(yas/initialize)
+;;(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+(yas/global-mode 1)

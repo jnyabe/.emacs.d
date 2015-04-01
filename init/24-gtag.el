718d1496b9ab77e190ff1bbb9cc7f7682defc8c2
Jun Yabe <yabe@auckland.local>
2015-04-01 15:01:24 +0900

    Update helm-gtag settings

--- init/24-gtag.el
+++ init/24-gtag.el
@@ -14,3 +14,16 @@
 	     (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
 	     (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
 	     
+;; switch header/cpp files
+(defun helm-gtags-find-file-toggle-header-source ()
+  (interactive)
+  (defun replace-string-regexp (str from to)
+    (when (string-match from str)
+      (replace-match to nil nil str)))
+  (let* ((file-name
+   (file-name-nondirectory (buffer-file-name)))
+  (replaced-file-name
+   (or (replace-string-regexp file-name "\\.cpp$" ".h")
+       (replace-string-regexp file-name "\\.h$" ".cpp"))))
+    (unless replaced-file-name (error "not found..."))
+    (helm-gtags-find-files replaced-file-name)))

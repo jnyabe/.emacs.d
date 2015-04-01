7c7397f61a82a87b113470436425c921995d4f60
Jun Yabe <yabe@auckland.local>
2015-04-01 15:02:09 +0900

    Update psvn settings

--- init/25-psvn.el
+++ init/25-psvn.el
@@ -1,10 +1,14 @@
 (require 'psvn)
 
 (setq svn-status-verbose nil)
+(setq svn-status-hide-unmodified t)
+(setq svn-status-hide-unknown t)
 
+(setq ediff-split-window-function 'split-window-horizontally)
+(setq svn-status-ediff-delete-temporary-files t)
 ;; language setting
 (setq process-coding-system-alist '(("svn" . utf-8)))
 (setq default-file-name-coding-system 'utf-8)
 (setq svn-status-svn-file-coding-system 'utf-8)
-
+(setq svn-status-svn-process-coding-system 'utf-8)
 
